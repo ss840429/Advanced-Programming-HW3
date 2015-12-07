b41651f082bde0fc3e832854982bb00bc388d7e8
Wlogsky <ss840429@gmail.com>
2015-12-08 01:38:22 +0800

    Operator +=

--- mystring.cpp
+++ mystring.cpp
@@ -21,9 +21,27 @@ namespace mystr
     const char& String::operator []( size_t idx ) const{
         return str_[idx] ;
     }
-
-
-
+    String& String::operator += ( const char* str ) {
+        int length = strlen( str ) ;
+        if( size_ + length >= capacity_ ){
+            char* newstr = new char[size_+length+1]() ;
+            size_ = size_+length ;
+            capacity_ = size_+1 ;
+            strcpy( newstr , str_ ) ;
+            delete[] this->str_ ;
+            str_ = newstr ;
+        }
+        strcat( str_ , str ) ;
+        return *this ;
+    }
+    String& String::operator += ( const String& str ){
+        (*this) += str.c_str() ;
+        return *this ;
+    }
+    String& String::operator += ( const char c ) {
+        char tmp[] = { c , '\0' } ;
+        (*this) += tmp ;
+        return *this ;
+    }
 
 }
-
